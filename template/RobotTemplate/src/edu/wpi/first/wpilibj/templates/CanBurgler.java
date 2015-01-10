fde16a9beeceaeac0565d3b2acbc7a09a8a01c76
kngkeeper <kngkeeper@gmail.com>
2015-01-10 12:10:44 -0500

    added retract to CanBurgler
    Basic template code for CanBurgler (will expand later)

---
+++ template/RobotTemplate/src/edu/wpi/first/wpilibj/templates/CanBurgler.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Jaguar;
+
+/**
+ *
+ * @author Alex
+ */
+public class CanBurgler {
+
+    Jaguar deathTrap;
+    
+    public CanBurgler () {
+        deathTrap = new Jaguar(9);
+    }
+    
+    /**
+     * retracts can burgler
+     */
+    public void retract() {
+        // FIRE ZE RETRACTOR!!!
+    }
+}
