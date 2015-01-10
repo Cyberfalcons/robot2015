be0c48d081328a49108ae41328e11cf93eed1c15
kngkeeper <kngkeeper@gmail.com>
2015-01-10 15:57:04 -0500

    Added basic BinElevator functions
    As we don't yet know how it works, we really can't implement most of
    this. We need to COME BACK later.

---
+++ template/RobotTemplate/src/edu/wpi/first/wpilibj/templates/BinElevator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Jaguar;
+
+/**
+ *
+ * @author Alex
+ */
+public class BinElevator {
+     //TODO: move to VariableMap
+   double chainSpeed = 1;
+    Jaguar binElevator;
+    
+    //TODO: figure out how bin elevator works and do it
+    
+    public BinElevator () {
+        binElevator = new Jaguar (4);
+    }
+    public void raiseBin () {
+    // TODO: 
+    }
+    private void setChainUp () {
+        binElevator.set (chainSpeed);
+        
+    }
+    private void setChainDown () {
+        binElevator.set (-chainSpeed);
+    }
+    
+}
