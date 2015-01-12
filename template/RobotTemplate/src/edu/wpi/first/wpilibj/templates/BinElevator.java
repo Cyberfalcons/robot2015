9021b8029736d1df7808ab938854e4df74d597ce
kngkeeper <kngkeeper@gmail.com>
2015-01-12 18:41:34 -0500

    Finished binelevator

--- template/RobotTemplate/src/edu/wpi/first/wpilibj/templates/BinElevator.java
+++ template/RobotTemplate/src/edu/wpi/first/wpilibj/templates/BinElevator.java
@@ -5,33 +5,38 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Jaguar;
 
 /**
  *
  * @author Alex
- * @author Dylan
  */
 public class BinElevator {
-	Jaguar binElevator;
-
-	//TODO: figure out how bin elevator works and do it
-
-	public BinElevator() {
-		binElevator = new Jaguar(VariableMap.PWM_BIN_ELEVATOR);
-	}
-	
-	public void raiseBin() {
-	// TODO: 
-	}
-	
-	private void setChainUp() {
-		binElevator.set(VariableMap.BIN_ELEVATOR_CHAIN_SPEED);
-		
-	}
-	
-	private void setChainDown() {
-		binElevator.set(-VariableMap.BIN_ELEVATOR_CHAIN_SPEED);
-	}
+     //TODO: move to VariableMap
+   double chainSpeed = 1;
+    Jaguar binElevator;
+    Encoder encChain;
+   
     
+    
+    //TODO: figure out how bin elevator works and do it
+    
+    public BinElevator () {
+        binElevator = new Jaguar (4);
+    }
+    public void raiseBin () {
+    // TODO: 
+    }
+    private void setChainUp () {
+        binElevator.set (chainSpeed);
+        
+    }
+    private void setChainDown () {
+        binElevator.set (-chainSpeed);
+        
+    }
+    public int getEncoderEnc () {
+        return encChain.get ();
+    }
 }
