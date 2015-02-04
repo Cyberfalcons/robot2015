5ffe1f817d6213b3e58e29cdb745a9f8bca2f1ad
xNovax <nova1104@live.com>
2015-02-03 19:25:44 -0500

    Added Controller Class

---
+++ 2015robot/src/org/usfirst/frc/team3710/robot/Controller.java
@@ -0,0 +1,175 @@
+package org.usfirst.frc.team3710.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Controller {
+
+	public Controller() {
+
+	}
+
+	double xAxis;
+	double yAxis;
+
+	boolean rightButton1;
+	boolean rightButton2;
+	boolean rightButton3;
+	boolean rightButton4;
+	boolean rightButton5;
+	boolean rightButton6;
+	boolean rightButton7;
+	boolean rightButton8;
+	boolean rightButton9;
+	boolean rightButton10;
+	boolean rightButton11;
+
+	boolean leftButton1;
+	boolean leftButton2;
+	boolean leftButton3;
+	boolean leftButton4;
+	boolean leftButton5;
+	boolean leftButton6;
+	boolean leftButton7;
+	boolean leftButton8;
+	boolean leftButton9;
+	boolean leftButton10;
+	boolean leftButton11;
+
+	boolean btnA;
+	boolean btnB;
+	boolean btnX;
+	boolean btnY;
+	boolean btnLB;
+	boolean btnRB;
+	boolean btnBACK;
+	boolean btnSTART;
+
+	public boolean getRightButton1() {
+		return rightButton1;
+	}
+
+	public boolean getRightButton2() {
+		return rightButton2;
+	}
+
+	public boolean getRightButton3() {
+		return rightButton3;
+	}
+
+	public boolean getRightButton4() {
+		return rightButton4;
+	}
+
+	public boolean getRightButton5() {
+		return rightButton5;
+	}
+
+	public boolean getRightButton6() {
+		return rightButton6;
+	}
+
+	public boolean getRightButton7() {
+		return rightButton7;
+	}
+
+	public boolean getRightButton8() {
+		return rightButton8;
+	}
+
+	public boolean getRightButton9() {
+		return rightButton9;
+	}
+
+	public boolean getRightButton10() {
+		return rightButton10;
+	}
+
+	public boolean getRightButton11() {
+		return rightButton11;
+	}
+
+	public boolean getLeftButton1() {
+		return leftButton1;
+	}
+
+	public boolean getLeftButton2() {
+		return leftButton2;
+	}
+
+	public boolean getLeftButton3() {
+		return leftButton3;
+	}
+
+	public boolean getLeftButton4() {
+		return leftButton4;
+	}
+
+	public boolean getLeftButton5() {
+		return leftButton5;
+	}
+
+	public boolean getLeftButton6() {
+		return leftButton6;
+	}
+
+	public boolean getLeftButton7() {
+		return leftButton7;
+	}
+
+	public boolean getLeftButton8() {
+		return leftButton8;
+	}
+
+	public boolean getLeftButton9() {
+		return leftButton9;
+	}
+
+	public boolean getLeftButton10() {
+		return leftButton10;
+	}
+
+	public boolean getLeftButton11() {
+		return leftButton11;
+	}
+
+	public boolean getBtnX() {
+		return btnX;
+	}
+
+	public boolean getBtnY() {
+		return btnY;
+	}
+
+	public boolean getBtnA() {
+		return btnA;
+	}
+
+	public boolean getBtnB() {
+		return btnB;
+	}
+
+	public boolean getBtnLB() {
+		return btnLB;
+	}
+
+	public boolean getBtnRB() {
+		return btnRB;
+	}
+
+	public boolean getBtnBACK() {
+		return btnBACK;
+	}
+
+	public boolean getBtnSTART() {
+		return btnSTART;
+	}
+
+	public double getXAxis() {
+		return xAxis;
+	}
+
+	public double getYAxis() {
+		return yAxis;
+	}
+
+}
