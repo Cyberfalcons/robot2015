571df3e6855a7a89d31148399595349375ddcac6
xNovax <nova1104@live.com>
2015-02-05 20:59:39 -0500

    Added solenoid ports to Variable Map and added methods to pinchclaw

--- 2015robot/src/org/usfirst/frc/team3710/robot/PinchClaw.java
+++ 2015robot/src/org/usfirst/frc/team3710/robot/PinchClaw.java
@@ -3,8 +3,21 @@ package org.usfirst.frc.team3710.robot;
 public class PinchClaw {
 
 	VariableMap map;
+	boolean isClawOpen = true;
 
 	public PinchClaw(VariableMap vm) {
 		map = vm;
+	}
+
+	public void openClaw() {
+
+	}
+
+	public void closeClaw() {
+
+	}
+
+	public boolean getIfClawOpen() {
+		return isClawOpen;
 	}
 }
--- 2015robot/src/org/usfirst/frc/team3710/robot/VariableMap.java
+++ 2015robot/src/org/usfirst/frc/team3710/robot/VariableMap.java
@@ -3,52 +3,66 @@ package org.usfirst.frc.team3710.robot;
 public class VariableMap {
 	// A file for constants used in the robot code
 
-	/* PWM Ports (motor outputs)
-	 *
-	 * Port 0 - Drive Left
-	 * Port 1 - Drive Right
-	 * Port 2 - Left Roller
-	 * Port 3 - Right Roller
-	 * Port 4 - Bin Elevator Main Chain
-	 * Port 5 - Tote Elevator Main Chain
-	 * Port 6 -
-	 * Port 7 -
-	 * Port 8 -
-	 * Port 9 - Can Burglar Retract
+	/*
+	 * PWM Ports (motor outputs)
+	 * 
+	 * Port 0 - Drive Left 
+	 * Port 1 - Drive Right 
+	 * Port 2 - Left Roller 
+	 * Port 3 - Right Roller 
+	 * Port 4 - Bin Elevator Main Chain 
+	 * Port 5 - Tote Elevator Main Chain 
+	 * Port 6 - 
+	 * Port 7 - 
+	 * Port 8 - 
+	 * Port 9 - 
 	 */
-	 public static final int PWM_DRIVE_LEFT = 0;
-	 public static final int PWM_DRIVE_RIGHT = 1;
-	 public static final int PWM_ROLLER_LEFT = 2;
-	 public static final int PWM_ROLLER_RIGHT = 3;
-	 public static final int PWM_BIN_ELEVATOR = 4;
-	 public static final int PWM_TOTE_ELEVATOR = 5;
-	 public static final int PWM_CAN_BURGLAR = 9;
-	 
-	 
-	/* Digital IO Pins
-	 *
-	 * Port 0 - Drive Encoder Left A
-	 * Port 1 - Drive Encoder Left B
-	 * Port 2 - Drive Encoder Right A
-	 * Port 3 - Drive Encoder Right B
-	 * Port 4 - Bin Zeroing Bottom
-	 * Port 5 - Bin Zeroing Top
-	 * Port 6 - Bin Elevator Encoder A
-	 * Port 7 - Bin Elevator Encoder B
+	public static final int PWM_DRIVE_LEFT = 0;
+	public static final int PWM_DRIVE_RIGHT = 1;
+	public static final int PWM_ROLLER_LEFT = 2;
+	public static final int PWM_ROLLER_RIGHT = 3;
+	public static final int PWM_BIN_ELEVATOR = 4;
+	public static final int PWM_TOTE_ELEVATOR = 5;
+
+	/*
+	 * Digital IO Pins
+	 * 
+	 * Port 0 - Drive Encoder Left A 
+	 * Port 1 - Drive Encoder Left B 
+	 * Port 2 - Drive Encoder Right A 
+	 * Port 3 - Drive Encoder Right B 
+	 * Port 4 - Bin Zeroing Bottom 
+	 * Port 5 - Bin Zeroing Top 
+	 * Port 6 - Bin Elevator Encoder A 
+	 * Port 7 - Bin Elevator Encoder B 
 	 * Port 8 - Tote Elevator Bottom 
 	 * Port 9 - Tote Elevator Top
 	 */
-	 public static final int DIO_DRIVE_ENC_LEFT_A = 0;
-	 public static final int DIO_DRIVE_ENC_LEFT_B = 1;
-	 public static final int DIO_DRIVE_ENC_RIGHT_A = 2;
-	 public static final int DIO_DRIVE_ENC_RIGHT_B = 3;
-	 public static final int DIO_BIN_ZEROING_TOP = 4;
-	 public static final int DIO_BIN_ZEROING_BOTTOM = 5;
-	 public static final int DIO_BIN_ELEVATOR_ENC_A = 6;
-	 public static final int DIO_BIN_ELEVATOR_ENC_B = 7;
-	 public static final int DIO_TOTE_ELEVATOR_BOTTOM = 8;
-	 public static final int DIO_TOTE_ELEVATOR_TOP = 9;
+	public static final int DIO_DRIVE_ENC_LEFT_A = 0;
+	public static final int DIO_DRIVE_ENC_LEFT_B = 1;
+	public static final int DIO_DRIVE_ENC_RIGHT_A = 2;
+	public static final int DIO_DRIVE_ENC_RIGHT_B = 3;
+	public static final int DIO_BIN_ZEROING_TOP = 4;
+	public static final int DIO_BIN_ZEROING_BOTTOM = 5;
+	public static final int DIO_BIN_ELEVATOR_ENC_A = 6;
+	public static final int DIO_BIN_ELEVATOR_ENC_B = 7;
+	public static final int DIO_TOTE_ELEVATOR_BOTTOM = 8;
+	public static final int DIO_TOTE_ELEVATOR_TOP = 9;
 
+	/*
+	 * Solenoid Ports 
+	 * Port 0 - Can Burglar Retractor 
+	 * Port 1 - 
+	 * Port 2 - 
+	 * Port 3 -
+	 * Port 4 - 
+	 * Port 5 - 
+	 * Port 6 - 
+	 * Port 7 - 
+	 * Port 8 - 
+	 * Port 9 -
+	 */
+	public static final int SOL_CAN_BURGLAR = 0;
 
 	/* Misc useful constants */
 	public static final double BIN_ELEVATOR_CHAIN_SPEED = 1.0;
