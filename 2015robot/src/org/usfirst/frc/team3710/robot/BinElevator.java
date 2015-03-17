47121e1b846cf4a46995b75750f1a1d4cc707cfb
xNovax <nova1104@live.com>
2015-03-17 12:50:46 -0400

    Fixing up some code issues

--- 2015robot/src/org/usfirst/frc/team3710/robot/BinElevator.java
+++ 2015robot/src/org/usfirst/frc/team3710/robot/BinElevator.java
@@ -3,14 +3,13 @@ package org.usfirst.frc.team3710.robot;
 import edu.wpi.first.wpilibj.*;
 
 public class BinElevator {
-	Victor binElevator;	
+	Victor binElevator;
 	Encoder encChain;
 	DigitalInput top;
 	DigitalInput bottom;
 	PIDController pid;
 	int position = 0;
 	int topLocation = 0;
-	boolean sensorsDisabled = false;
 
 	public BinElevator(Victor v, Encoder e, DigitalInput to, DigitalInput b, PIDController p) {
 		binElevator = v;
@@ -26,59 +25,49 @@ public class BinElevator {
 		encChain.setSamplesToAverage(7);
 	}
 
-	public void setPositionUp() {
-		if(sensorsDisabled != true){
-			pid.enable();
-			if (getTop() == false) {
-				position = position + 40;
-				pid.setSetpoint(position);
-			} else if (getTop() == true) {
-				topLocation = getEncoder();
-				position = position - 20;
-				pid.setSetpoint(position);
+	public void setPIDPositionUp() {
+		pid.enable();
+		if (getTop() == false) {
+			position = position + 40;
+			pid.setSetpoint(position);
+		} else if (getTop() == true) {
+			topLocation = getEncoder();
+			position = position - 20;
+			pid.setSetpoint(position);
 
-				if (VariableMap.VERBOSE_CONSOLE) {
-					System.out.println("ELEVATOR UP DISABLED BY LIMIT SWITCH!");
-				}
+			if (VariableMap.VERBOSE_CONSOLE) {
+				System.out.println("ELEVATOR UP DISABLED BY LIMIT SWITCH!");
 			}
-		}else if(sensorsDisabled){
-			pid.disable();
-			binElevator.set(0.5);
 		}
 	}
 
-	public void setPositionDown() {
-		if(sensorsDisabled != true){
-			pid.enable();
-			if (getBottom() == false) {
-				position = position - 40;
-				pid.setSetpoint(position);
-			} else if (getBottom() == true) {
-				resetEncoder();
-				if (VariableMap.VERBOSE_CONSOLE) {
-					System.out.println("ELEVATOR DOWN DISABLED BY LIMIT SWITCH");
-				}
+	public void setPIDPositionDown() {
+		pid.enable();
+		if (getBottom() == false) {
+			position = position - 40;
+			pid.setSetpoint(position);
+		} else if (getBottom() == true) {
+			resetEncoder();
+			if (VariableMap.VERBOSE_CONSOLE) {
+				System.out.println("ELEVATOR DOWN DISABLED BY LIMIT SWITCH");
 			}
-		}else if(sensorsDisabled){
-			pid.disable();
-			binElevator.set(-0.5);
 		}
 	}
 	
-	public void stopElevator(){
+	public void setPIDPositionZero(){
+		
+	}
+
+	public void stopElevator() {
 		binElevator.set(0.0);
 	}
 
 	public int getEncoder() {
 		return encChain.get();
 	}
-	
-	public int getPIDPosition(){
+
+	public int getPIDPosition() {
 		return position;
-	}
-	
-	public int getTopLocation(){
-		return topLocation;
 	}
 
 	public void setSetPoint(int point) {
@@ -96,24 +85,10 @@ public class BinElevator {
 	public boolean getBottom() {
 		return bottom.get();
 	}
-	
-	public void setSensorsDisabled(boolean b){
-		sensorsDisabled = b;
-		if(sensorsDisabled == true){
-			pid.disable();
-			position = 0;
-		}
-	}
-	
-	public boolean getSensorsDisabled(){
-		return sensorsDisabled;
-	}
-	
-	public void disablePID(){
+
+	public void disablePID() {
 		pid.disable();
-	}
-	
-	public void resetPosition(){
 		position = 0;
+		binElevator.set(0.0);
 	}
 }
--- 2015robot/src/org/usfirst/frc/team3710/robot/Robot.java
+++ 2015robot/src/org/usfirst/frc/team3710/robot/Robot.java
@@ -13,6 +13,8 @@ public class Robot extends IterativeRobot {
 	RollerClaw rollerClaw;
 	CanBurglar_v3 canBurglar;
 	PowerDistributionPanel pdp;
+	Lamp lamp;
+	Ramp ramp;
 
 	// Controller and Sensor
 	Talon driveRightTalonA, driveLeftTalonA;
@@ -37,30 +39,33 @@ public class Robot extends IterativeRobot {
 		// Drive
 		driveLeftTalonA = new Talon(VariableMap.PWM_DRIVE_LEFT_A);
 		driveRightTalonA = new Talon(VariableMap.PWM_DRIVE_RIGHT_A);
-		encDriveLeft = new Encoder(VariableMap.DIO_DRIVE_ENC_LEFT_A, VariableMap.DIO_DRIVE_ENC_LEFT_B, false, Encoder.EncodingType.k4X);
-		encDriveRight = new Encoder(VariableMap.DIO_DRIVE_ENC_RIGHT_A, VariableMap.DIO_DRIVE_ENC_RIGHT_B, false, Encoder.EncodingType.k4X);
-		driveLeftPID = new PIDController(VariableMap.DRIVE_PID_P, VariableMap.DRIVE_PID_I, VariableMap.DRIVE_PID_D,encDriveLeft, driveLeftTalonA);
-		driveRightPID = new PIDController(VariableMap.DRIVE_PID_P, VariableMap.DRIVE_PID_I, VariableMap.DRIVE_PID_D, encDriveRight, driveRightTalonA);;
+		encDriveLeft = new Encoder(VariableMap.DIO_DRIVE_ENC_LEFT_A,VariableMap.DIO_DRIVE_ENC_LEFT_B, false,Encoder.EncodingType.k4X);
+		encDriveRight = new Encoder(VariableMap.DIO_DRIVE_ENC_RIGHT_A,VariableMap.DIO_DRIVE_ENC_RIGHT_B, false,Encoder.EncodingType.k4X);
+		driveLeftPID = new PIDController(VariableMap.DRIVE_PID_P,VariableMap.DRIVE_PID_I, VariableMap.DRIVE_PID_D, encDriveLeft,
+				driveLeftTalonA);
+		driveRightPID = new PIDController(VariableMap.DRIVE_PID_P,VariableMap.DRIVE_PID_I, VariableMap.DRIVE_PID_D,encDriveRight, driveRightTalonA);
 
 		// Bin Elevator
 		binElevatorVictor = new Victor(VariableMap.PWM_BIN_ELEVATOR);
-		binElevatorEncoder = new Encoder(VariableMap.DIO_BIN_ELEVATOR_ENC_A, VariableMap.DIO_BIN_ELEVATOR_ENC_B, false, Encoder.EncodingType.k4X);
+		binElevatorEncoder = new Encoder(VariableMap.DIO_BIN_ELEVATOR_ENC_A,VariableMap.DIO_BIN_ELEVATOR_ENC_B, false,Encoder.EncodingType.k4X);
 		binElevatorTop = new DigitalInput(VariableMap.DIO_BIN_ELEVATOR_TOP);
 		binElevatorBottom = new DigitalInput(VariableMap.DIO_BIN_ELEVATOR_BOTTOM);
-		binElevatorPID = new PIDController(binElevatorPIDP, binElevatorPIDI, binElevatorPIDD, binElevatorEncoder, binElevatorVictor);
+		binElevatorPID = new PIDController(binElevatorPIDP, binElevatorPIDI,binElevatorPIDD, binElevatorEncoder, binElevatorVictor);
 
 		// Roller Claw
 		rollerClawRightVictor = new Victor(VariableMap.PWM_ROLLER_RIGHT);
 		rollerClawLeftVictor = new Victor(VariableMap.PWM_ROLLER_LEFT);
-		
+
 		// Can Burglar
-		canBurglarSolenoid = new DoubleSolenoid(VariableMap.PCM_CAN_BURGLAR_SOLE_1, VariableMap.PCM_CAN_BURGLAR_SOLE_2);
+		canBurglarSolenoid = new DoubleSolenoid(VariableMap.PCM_CAN_BURGLAR_SOLE_1,VariableMap.PCM_CAN_BURGLAR_SOLE_2);
 
 		// Systems
-		drive = new Drive(driveLeftTalonA, driveRightTalonA, encDriveLeft, encDriveRight, driveLeftPID, driveRightPID);
-		binElevator = new BinElevator(binElevatorVictor, binElevatorEncoder, binElevatorTop, binElevatorBottom, binElevatorPID);
+		drive = new Drive(driveLeftTalonA, driveRightTalonA, encDriveLeft,encDriveRight, driveLeftPID, driveRightPID);
+		binElevator = new BinElevator(binElevatorVictor, binElevatorEncoder,binElevatorTop, binElevatorBottom, binElevatorPID);
 		rollerClaw = new RollerClaw(rollerClawLeftVictor, rollerClawRightVictor);
 		canBurglar = new CanBurglar_v3(canBurglarSolenoid);
+		lamp = new Lamp();
+		ramp = new Ramp();
 		driverControl = new JoystickControllerWrapper(0, 1);
 		operatorControl = new XBoxControllerWrapper(2);
 		pdp = new PowerDistributionPanel();
@@ -78,24 +83,24 @@ public class Robot extends IterativeRobot {
 		timer.start();
 		binElevatorPID.setPID(0.018, 0.0, 0.006);
 	}
-	
+
 	public void autonomousPeriodic() {
-		if(autoOn == true){
+		if (autoOn == true) {
 			System.out.println("DRIVE LEFT: " + drive.getEncoderLeft());
-			System.out.println("DRIVE RIGHT: " +  drive.getEncoderRight());
-			
-			if((timer.get() > 0.25) && (timer.get() < 0.5)){
-				binElevator.setPositionUp();
-			}else if((timer.get() < 0.75) && (timer.get() > 0.5)){
-				binElevator.setPositionDown();
-			}else if((timer.get() > 1.25)){
+			System.out.println("DRIVE RIGHT: " + drive.getEncoderRight());
+
+			if ((timer.get() > 0.25) && (timer.get() < 0.5)) {
+				binElevator.setPIDPositionUp();
+			} else if ((timer.get() < 0.75) && (timer.get() > 0.5)) {
+				binElevator.setPIDPositionDown();
+			} else if ((timer.get() > 1.25)) {
 				binElevator.disablePID();
 			}
-			
-			if((timer.get() > 2.90) && (timer.get() < 5.00)){
+
+			if ((timer.get() > 2.90) && (timer.get() < 5.00)) {
 				drive.setPIDDriveLeft(-70);
 				drive.setPIDDriveRight(70);
-			}else if((timer.get() > 5.1)){
+			} else if ((timer.get() > 5.1)) {
 				drive.setPIDDriveLeft(390);
 				drive.setPIDDriveRight(-390);
 			}
@@ -107,8 +112,7 @@ public class Robot extends IterativeRobot {
 		drive.disableRightPIDControl();
 		binElevator.resetEncoder();
 		binElevator.disablePID();
-		binElevator.resetPosition();
-		binElevatorPID.setPID(VariableMap.BIN_ELEVATOR_PID_P, VariableMap.BIN_ELEVATOR_PID_I, VariableMap.BIN_ELEVATOR_PID_D);
+		binElevatorPID.setPID(VariableMap.BIN_ELEVATOR_PID_P,VariableMap.BIN_ELEVATOR_PID_I, VariableMap.BIN_ELEVATOR_PID_D);
 	}
 
 	public void teleopPeriodic() {
@@ -116,11 +120,11 @@ public class Robot extends IterativeRobot {
 		doBinElevator();
 		doRollerClaw();
 		doCanBurglar();
-		
+
 		updateValuesFromSmartDashboard();
-		
-		if(VariableMap.VERBOSE_CONSOLE){
-			System.out.println("Elevator Encoder: " +  binElevator.getEncoder());
+
+		if (VariableMap.VERBOSE_CONSOLE) {
+			System.out.println("Elevator Encoder: " + binElevator.getEncoder());
 			System.out.println("Elevator Top: " + binElevator.getTop());
 			System.out.println("Elevator Bottom: " + binElevator.getBottom());
 			System.out.println("DRIVE LEFT: " + drive.getEncoderLeft());
@@ -131,9 +135,9 @@ public class Robot extends IterativeRobot {
 	public void testPeriodic() {
 
 	}
-	
-	public void disabledInit(){
-		
+
+	public void disabledInit() {
+
 	}
 
 	public void disabledPeriodic() {
@@ -142,63 +146,43 @@ public class Robot extends IterativeRobot {
 
 	public void doDrive() {
 		driverControl.checkFlip();
-		
-		if(operatorControl.getBtnA())
-		{
+
+		if (operatorControl.getBtnA()) {
 			drive.flipSlowMode();
 		}
-		
+
 		drive.setDriveRight(driverControl.driveRight());
 		drive.setDriveLeft(driverControl.driveLeft());
 	}
 
 	public void doRollerClaw() {
-		
-		if(operatorControl.getBtnB()){
+
+		if (operatorControl.getBtnB()) {
 			rollerClaw.flipSlowMode();
 		}
-		
+
 		if (driverControl.rollerIn()) {
 			rollerClaw.binIn();
-		}else if(driverControl.rollerOut()) {
+		} else if (driverControl.rollerOut()) {
 			rollerClaw.binOut();
-		} else  if(operatorControl.getBtnB()){
+		} else if (operatorControl.getBtnB()) {
 			rollerClaw.binClockWise();
-		}else {
+		} else {
 			rollerClaw.stop();
 		}
 	}
 
 	public void doCanBurglar() {
-		
+
 	}
 
 	public void doBinElevator() {
-		if(binElevator.getSensorsDisabled() == false){
-			if (driverControl.elevatorUp()) {
-				binElevator.setPositionUp();
-			} else if (driverControl.elevatorDown()) {
-				binElevator.setPositionDown();
-			}else if(driverControl.getRightButton11()){
-				binElevator.setSetPoint(0);
-			}
-		}else if(binElevator.getSensorsDisabled() == true){
-			if(driverControl.elevatorUp()){
-				binElevator.setPositionUp();
-			}else if(driverControl.elevatorDown()){
-				binElevator.setPositionDown();
-			}else{
-				binElevator.stopElevator();
-			}
-		}
-		
-		if(driverControl.getRightTrigger()){
-			if(binElevator.getSensorsDisabled() == true){
-				binElevator.setSensorsDisabled(false);
-			}
-			else if (binElevator.getSensorsDisabled() == false){
-				binElevator.setSensorsDisabled(true);
-			}
+		if (driverControl.elevatorUp()) {
+			binElevator.setPIDPositionUp();
+		} else if (driverControl.elevatorDown()) {
+			binElevator.setPIDPositionDown();
+		} else if (driverControl.getRightButton11()) {
+			binElevator.setSetPoint(0);
 		}
 	}
 
@@ -216,16 +200,16 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putDouble("Bin Elevator PID P", binElevatorPIDP);
 		SmartDashboard.putDouble("Bin Elevator PID I", binElevatorPIDI);
 		SmartDashboard.putDouble("Bin Elevator PID D", binElevatorPIDD);
-		
+
 		SmartDashboard.putBoolean("Bin Elevator Top", binElevator.getTop());
-		SmartDashboard.putBoolean("Bin Elevator Bottom", binElevator.getBottom());
+		SmartDashboard.putBoolean("Bin Elevator Bottom",binElevator.getBottom());
 	}
 
 	@SuppressWarnings("deprecation")
 	private void initializeSmartDashboard() {
 		autoChooser = new SendableChooser();
 		autoChooser.addDefault("Release CanBurglar, Drive Forwards, Drive Backwards", 0);
-		
+
 		SmartDashboard.putData("Autonomous Chooser", autoChooser);
 
 		SmartDashboard.putDouble("Left Encoder", encDriveLeft.get());
@@ -235,8 +219,8 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putDouble("Bin Elevator PID P", binElevatorPIDP);
 		SmartDashboard.putDouble("Bin Elevator PID I", binElevatorPIDI);
 		SmartDashboard.putDouble("Bin Elevator PID D", binElevatorPIDD);
-		
+
 		SmartDashboard.putBoolean("Bin Elevator Top", binElevator.getTop());
-		SmartDashboard.putBoolean("Bin Elevator Bottom", binElevator.getBottom());
+		SmartDashboard.putBoolean("Bin Elevator Bottom",binElevator.getBottom());
 	}
 }
